5b17978418a2037ccf07fd9ce9f37e323570cfb7
zun1804 <82303020+zun1804@users.noreply.github.com>
2021-12-13 14:42:09 +0700

    Update tonghieutichthuong.c

--- tonghieutichthuong.c
+++ tonghieutichthuong.c
@@ -1,10 +1,5 @@
 #include"tonghieutichthuong.h"
-/*huong di de chuyen chuoi thanh mot da thuc dang mang.
-1.nhap chuoi chua ca khoang trang => nhapXuatChuoiDaThuc
-2.xoa khoang trang de xu ly chuoi de dang hon => can co ham xoa mot ky tu => xoaKhoangTrang & xoaMotKyTu
-3.vi nguoi dung nhap vao dang...+ X + heso+... => them ^1 vao sau X va them X^0 vao sau heso => dathucStupid.
-4.xet duyet chuoi tuong tu nhu mang de lay ra heso vs so mu =>daThuc
-5. da co da thuc, tien hanh thuc hien cac phep tinh*/
+
 
 void nhapXuatChuoiDaThuc(char chuoiDaThuc[]){
 	printf("nhap vao chuoi da thuc: "); 
